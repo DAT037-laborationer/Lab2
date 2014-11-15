33d104715fa1d6e62fbf7e224e8f776406b5fdb6
Hampus L. (Skinkan) <lidin@student.chalmers.se>
2014-11-15 15:37:53 +0100

    Added code for implementation of a binomial heap data structure. Merge is yet to be tested.

--- PriorityQueue.hs
+++ PriorityQueue.hs
@@ -60,3 +60,49 @@ removeMin Empty = error "IllegalStateException"
 removeMin (Node _ l r )   = restore b bt l 
                             where (b,bt) = getMostRight r
 
+
+{-
+data Node a b = Node a [b] 
+  deriving (Eq,Ord,Show)
+
+data BinHeap a = Empty |
+                 Heap [Node a (BinHeap a)]
+  deriving (Eq,Ord,Show)
+
+-- | Returns an empty queue.
+emptyQueue :: BinHeap a
+emptyQueue = Empty
+
+-- | Returns True if queue is empty, False otherwise.
+isEmpty :: BinHeap a -> Bool
+isEmpty Empty = True
+isEmpty _     = False
+
+-- | Returns the value of the order of a queue.
+order :: BinHeap a -> Int
+order (Heap [Node _ []]) = 0 
+order (Heap [Node _ ns]) = length ns
+order _                  = undefined
+
+-- | Merges two queues into one.
+merge :: Ord a => (a -> a -> Bool) -> BinHeap (a,String) -> BinHeap (a,String)
+           -> BinHeap (a,String)
+merge _ t1 Empty = t1
+merge _ Empty t2 = t2
+merge f t1@(Heap [Node a tt1]) t2@(Heap [Node b tt2])
+  | order t1 == order t2 = if f (fst a) (fst b)
+                           then Heap [Node a ([t2] ++ tt1)]
+                           else Heap [Node b ([t1] ++ tt2)]
+  | order t1 < order t2  = Heap [Node b ((merge f t1 (head tt2)):(tail tt2))]
+  | otherwise            = Heap [Node a ((merge f t2 (head tt1)):(tail tt1))]
+
+-- | Adds an bid to a queue.
+addBid :: Ord a => (a,String) -> BinHeap (a,String) -> BinHeap (a,String)
+addBid a Empty = Heap [Node a []] 
+addBid a t = merge (>) (Heap [Node a []]) t
+
+-- | Adds an ask to a queue.
+addAsk :: Ord a => (a,String) -> BinHeap (a,String) -> BinHeap (a,String)
+addAsk a Empty = Heap [Node a []] 
+addAsk a t = merge (<) (Heap [Node a []]) t
+-}
